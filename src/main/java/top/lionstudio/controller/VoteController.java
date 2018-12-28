2e1a5d29b5fd7f2b8f20701a01a31376aabbd56c
lion <695706553@qq.com>
2018-12-28 15:40:24 +0800

    asd

--- src/main/java/top/lionstudio/controller/VoteController.java
+++ src/main/java/top/lionstudio/controller/VoteController.java
@@ -2,26 +2,32 @@ package top.lionstudio.controller;
 
 import java.util.Map;
 
+import javax.persistence.Id;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import top.lionstudio.entity.VoteInfo;
+import top.lionstudio.repo.VoteInfoRepo;
+import top.lionstudio.tool.MapTool;
+
 
 @RestController
 public class VoteController {
+	@Autowired
+	private VoteInfoRepo voteInfoRepo;
 	
 	@RequestMapping(value = "/vote/getVoteById", method = RequestMethod.POST)
 	public @ResponseBody Object getVoteById(@RequestBody Map<String,Object> map, HttpSession httpsession) {
 		int id=(int) map.get("id");
-		
-		
-		
+		VoteInfo voteInfo=voteInfoRepo.findById(id);
 
-		return null;
+		return MapTool.getSuccessRes(voteInfo);
 	}
 	@RequestMapping(value = "/vote/createVote", method = RequestMethod.POST)
 	public @ResponseBody Object createVote(@RequestBody Map<String,Object> map, HttpSession httpsession) {
--- src/main/java/top/lionstudio/repo/VoteInfoRepo.java
+++ src/main/java/top/lionstudio/repo/VoteInfoRepo.java
@@ -1,5 +1,9 @@
 package top.lionstudio.repo;
 
+import java.util.List;
+
+import javax.persistence.Id;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +12,7 @@ import top.lionstudio.entity.VoteInfo;
 @Repository
 public interface VoteInfoRepo  extends JpaRepository<VoteInfo, Long>{
 	
+	public VoteInfo findById(int id);
 	
 
 	
